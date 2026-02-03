6a08a70ca91932f367dd7d6859a946d2792a05ce
strom314 <jakub.gros777@gmail.com>
2026-02-03 12:43:50 +0100

    refactor: restructure App component to implement routing with public and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LogIn from "./components/LogIn";
+import SignUp from "./components/SignUp";
+import Chat from "./components/Chat";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
-    <>
-      <h1>welcome to my app</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* public routes */}
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* protected routes */}
+        <Route
+          path="/chat"
+          element={
+            <ProtectedRoute>
+              <Chat />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
