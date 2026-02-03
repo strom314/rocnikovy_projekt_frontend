c2fc8e059bfa6e78e33d4db203c202debbf008b9
strom314 <jakub.gros777@gmail.com>
2026-02-03 13:18:03 +0100

    feat: write the signUp form markup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,8 +1,83 @@
+import { useState } from "react";
+
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errors, setErrors] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function handleSubmit() {}
+
   return (
-    <>
+    <div>
       <h1>sign up</h1>
-    </>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="confirmPassword">confirm password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            required
+            value={confirmPassword}
+            onChange={(e) => {
+              setConfirmPassword(e.target.value);
+            }}
+          />
+        </div>
+
+        {errors && <div>errors: {errors}</div>}
+
+        <button type="submit" disabled={loading}>
+          {loading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
   );
 }
 
