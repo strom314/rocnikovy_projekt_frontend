e96f01e37fd63008ea4258a743b5e36b091b7129
strom314 <jakub.gros777@gmail.com>
2026-02-06 15:56:58 +0100

    feat: add MessageInput component for sending messages in selected conversation. Upon sending it calls fetchMessges to reload the message list

---
+++ src/components/MessageInput.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+
+function MessageInput({ selectedConversationId, fetchMessages }) {
+  const { token } = useAuth();
+
+  const [message, setMessage] = useState("");
+
+  const [error, setError] = useState(null);
+
+  async function onMessageSend(e) {
+    e.preventDefault();
+    setError(null);
+
+    if (!selectedConversationId) {
+      setError("Select a conversation first");
+      return;
+    }
+
+    if (message.trim().length === 0) {
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `http://localhost:3000/conversation/${selectedConversationId}/messages`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message: message.trim() }),
+        },
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to send message");
+      }
+
+      fetchMessages();
+
+      setMessage("");
+    } catch (error) {
+      setError(error?.message ?? "Failed to send message");
+    }
+  }
+
+  return (
+    <form onSubmit={onMessageSend}>
+      {error && <p>{error}</p>}
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button type="submit">send</button>
+    </form>
+  );
+}
+
+export default MessageInput;
